baa71b3479a88c7b8dc853507b35824c0b8e3e79
astravsu <astravsu@gmail.com>
2024-03-05 23:08:47 +0300

    add role & employee tables

--- src/main/java/com/example/farm/model/Employee.java
+++ src/main/java/com/example/farm/model/Employee.java
@@ -1,13 +1,36 @@
 package com.example.farm.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+
 import java.util.Set;
 
+@Data
+@Table(name = "employee")
+@Entity
 public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employeeId;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "patronymic", nullable = false)
     private String patronymic;
+
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "employee_role",
+            joinColumns = {@JoinColumn(name = "employee_id")},
+            inverseJoinColumns = {@JoinColumn(name = "role_id")})
     private Set<Role> roles;
+
 
 }
--- src/main/java/com/example/farm/model/Role.java
+++ src/main/java/com/example/farm/model/Role.java
@@ -1,8 +1,19 @@
 package com.example.farm.model;
 
 import com.example.farm.model.enams.ERole;
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "role")
 
 public class Role {
-    private short roleId;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer roleId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(unique = true, nullable = false)
     private ERole name;
 }
