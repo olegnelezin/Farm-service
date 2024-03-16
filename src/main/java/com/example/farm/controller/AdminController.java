67da5b214b01ca8bdbe8cd44b8bcd6722bbed23f
astravsu <astravsu@gmail.com>
2024-03-16 08:59:36 +0300

    add javadoc

--- src/main/java/com/example/farm/controller/AdminController.java
+++ src/main/java/com/example/farm/controller/AdminController.java
@@ -71,6 +71,13 @@ public class AdminController {
         return new MessageDTO(productService.deleteProductByName(name));
     }
 
+    /**
+     Получение списка собранных товаров по конкретному работнику
+     @param request Данные о временном промежутке и работнике.
+     @return Ответ HTTP-запроса со статусом 200 и списком собранных товаров,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 400, с сообщением, "Invalid data type".
+     */
     @PostMapping("/collected-products/employee")
     public List<EmployeeCollectedProductDTO> getProductsByEmployee(@RequestBody GetCollectedProductsEmployeeRequest request) {
         List<EmployeeCollectedProductDTO> collectedProducts =
@@ -80,6 +87,13 @@ public class AdminController {
         return collectedProducts;
     }
 
+    /**
+     Получение списка собранных товаров по ферме
+     @param request Данные о временном промежутке.
+     @return Ответ HTTP-запроса со статусом 200 и списком собранных товаров,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 400, с сообщением, "Invalid data type".
+     */
     @PostMapping("/collected-products/farm")
     public List<FarmCollectedProductDTO> getProductsByFarm(@RequestBody GetCollectedProductsFarmRequest request) {
         List<FarmCollectedProductDTO> collectedProducts =
@@ -89,26 +103,68 @@ public class AdminController {
         return collectedProducts;
     }
 
+    /**
+     Установление оценки
+     @param request Данные о работнике и оценке.
+     @return Ответ HTTP-запроса со статусом 200, с сообщением, что оценка поставлена,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 404, с сообщением, что работника не существует,
+     или со статусом 409, с сообщением, что оценка уже поставлена.
+     */
     @PostMapping("/mark")
     public MessageDTO setMarkForEmployee(@RequestBody SetMarkRequest request) {
         return new MessageDTO(employeeMarkService.saveEmployeeMark(request));
     }
 
+    /**
+     Удаление оценки
+     @param request Данные о работнике.
+     @return Ответ HTTP-запроса со статусом 200, с сообщением, что оценка удалена,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 404, с сообщением, что работника не существует,
+     или со статусом 404, с сообщением, что оценка не существует.
+     */
     @DeleteMapping("/mark")
     public MessageDTO deleteMarkForEmployee(@RequestBody DeleteMarkRequest request) {
         return new MessageDTO(employeeMarkService.deleteEmployeeMark(request));
     }
 
+    /**
+     Установление плана
+     @param request Данные о работнике и плане.
+     @return Ответ HTTP-запроса со статусом 200, с сообщением, что план установлен,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 404, с сообщением, что работника не существует,
+     или со статусом 409, с сообщением, что план на продукт уже установлен.
+     */
     @PostMapping("/plan")
     public MessageDTO setPlanForEmployee(@RequestBody SetPlanRequest request) {
         return new MessageDTO(planedProductService.setEmployeePlan(request));
     }
 
+    /**
+     Удаление плана
+     @param request Данные о работнике.
+     @return Ответ HTTP-запроса со статусом 200, с сообщением, что план удален,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 404, с сообщением, что работника не существует,
+     или со статусом 404, с сообщением, что плана не существует.
+     */
     @DeleteMapping("/plan")
     public MessageDTO deletePlanForEmployee(@RequestBody DeletePlanRequest request) {
         return new MessageDTO(planedProductService.deleteEmployeePlan(request));
     }
 
+    /**
+     Изменение данных администратора
+     @param type Тип данных.(email/password).
+     @param authentication Данные об аутентификации.
+     @param request Новые данные.
+     @return Ответ HTTP-запроса со статусом 200, с сообщением, данные изменены,
+     или со статусом 403, с сообщением "Unauthorized",
+     или со статусом 404, с сообщением, что работника не существует,
+     или со статусом 400, с сообщением "Invalid data type".
+     */
     @PutMapping("/edit/{type}")
     public MessageDTO editCredentials(@PathVariable("type") String type,
                                       Authentication authentication,
