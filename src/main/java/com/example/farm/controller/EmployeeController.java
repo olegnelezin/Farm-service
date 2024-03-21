24b073f068646a9d88c492f4b0d0f32c87cf1105
astravsu <astravsu@gmail.com>
2024-03-21 19:08:59 +0300

    add requestmapping "/employee" in EmployeeController

--- src/main/java/com/example/farm/controller/EmployeeController.java
+++ src/main/java/com/example/farm/controller/EmployeeController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @AllArgsConstructor
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     private final CollectedProductService collectedProductService;
     private final RemainedPlanedProductsService remainedPlanedProductsService;
